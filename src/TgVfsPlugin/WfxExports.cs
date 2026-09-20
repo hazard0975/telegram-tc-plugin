15dfaa1d2598cb35e8d1f6776d8268a7554a3eb8
hazard0975 <v0975183223@gmail.com>
2026-09-20 07:35:02 +0300

    fix: update default view column widths
    Adjust column widths in FsContentGetDefaultView for better layout consistency in Total Commander.

--- src/TgVfsPlugin/WfxExports.cs
+++ src/TgVfsPlugin/WfxExports.cs
@@ -2692,7 +2692,7 @@ public static unsafe class WfxExports
     {
         string contents = "[=<fs>.VersionName]\\n[=tc.size]\\n[=tc.writedate]";
         string headers = "Версия\\nРазмер\\nДата";
-        string widths = "150,40,60,80,120";
+        string widths = "280,25,15,-40,-60";
         string options = "-1|0";
 
         CopyStringToPtrA(contents, viewContents, maxLen);
@@ -2707,7 +2707,7 @@ public static unsafe class WfxExports
     {
         string contents = "[=<fs>.VersionName]\\n[=tc.size]\\n[=tc.writedate]";
         string headers = "Версия\\nРазмер\\nДата";
-        string widths = "150,40,60,80,120";
+        string widths = "280,25,15,-40,-60";
         string options = "-1|0";
 
         CopyStringToPtrW(contents, viewContents, maxLen);
